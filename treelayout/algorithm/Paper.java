1d416c0d729b2d7a75866e7f3c75c8da619c5d50
alexander <alexander@archhome.fedora>
2024-03-18 14:30:37 -0500

    More verbose variable names

--- treelayout/algorithm/Paper.java
+++ treelayout/algorithm/Paper.java
@@ -3,162 +3,223 @@ package treelayout.algorithm;
 /* The extended Reingold-Tilford algorithm as described in the paper
  * "Drawing Non-layered Tidy Trees in Linear Time" by Atze van der Ploeg
  * Accepted for publication in Software: Practice and Experience, to Appear.
- * 
- * This code is in the public domain, use it any way you wish. A reference to the paper is 
- * appreciated!
  */
 
 public class Paper {
-	public static class Tree {
-		   double w, h;          // ^{\normalfont Width and height.}^
-		   double x, y, prelim, mod, shift, change;
-		   Tree tl, tr;          // ^{\normalfont Left and right thread.}^                        
-		   Tree el, er;          // ^{\normalfont Extreme left and right nodes.}^ 
-		   double msel, mser;    // ^{\normalfont Sum of modifiers at the extreme nodes.}^ 
-		   Tree[] c; int cs;     // ^{\normalfont Array of children and number of children.}^ 
-		   
-		   Tree(double w, double h, double y,Tree... c) {
-		      this.w = w; this.h = h; this.y = y; this.c = c; 
-		      this.cs = c.length;
-		   }
-		 }
-		static void layout(Tree t){ firstWalk(t); secondWalk(t,0); }
-		   
-		static  void firstWalk(Tree t){
-		  if(t.cs == 0){ setExtremes(t); return; }
-		  firstWalk(t.c[0]);
-		  // ^{\normalfont Create siblings in contour minimal vertical coordinate and index list.}^
-		  IYL ih =  updateIYL(bottom(t.c[0].el),0,null);                    
-		  for(int i = 1; i < t.cs; i++){
-		     firstWalk(t.c[i]);
-		     //^{\normalfont Store lowest vertical coordinate while extreme nodes still point in current subtree.}^
-		     double minY = bottom(t.c[i].er);                                
-		     seperate(t,i,ih);
-		     ih = updateIYL(minY,i,ih);                                     
-		  }
-		  positionRoot(t);
-		  setExtremes(t);
-		}
-		  
-		static  void setExtremes(Tree t) {
-		   if(t.cs == 0){
-		      t.el = t; t.er = t;
-		      t.msel = t.mser =0;
-		   } else {
-		      t.el = t.c[0].el; t.msel = t.c[0].msel;
-		      t.er = t.c[t.cs-1].er; t.mser = t.c[t.cs-1].mser;
-		   }
-		}
-		  
-		static void seperate(Tree t,int i,  IYL ih ){
-		   // ^{\normalfont Right contour node of left siblings and its sum of modfiers.}^  
-		   Tree sr = t.c[i-1]; double mssr = sr.mod;
-		   // ^{\normalfont Left contour node of current subtree and its sum of modfiers.}^  
-		   Tree cl = t.c[i]  ; double mscl = cl.mod;
-		   while(sr != null && cl != null){
-		      if(bottom(sr) > ih.lowY) ih = ih.nxt;                                
-		      // ^{\normalfont How far to the left of the right side of sr is the left side of cl?}^  
-		      double dist = (mssr + sr.prelim + sr.w) - (mscl + cl.prelim);
-		      if(dist > 0){
-		         mscl+=dist;
-		         moveSubtree(t,i,ih.index,dist);
-		      }
-		      double sy = bottom(sr), cy = bottom(cl);
-		      // ^{\normalfont Advance highest node(s) and sum(s) of modifiers}^  
-		      if(sy <= cy){                                                    
-		         sr = nextRightContour(sr);
-		         if(sr!=null) mssr+=sr.mod;
-		      }                                                               
-		      if(sy >= cy){                                                  
-		         cl = nextLeftContour(cl);
-		         if(cl!=null) mscl+=cl.mod;
-		      }                                                              
-		   }
-		   // ^{\normalfont Set threads and update extreme nodes.}^  
-		   // ^{\normalfont In the first case, the current subtree must be taller than the left siblings.}^  
-		   if(sr == null && cl != null) setLeftThread(t,i,cl, mscl);
-		   // ^{\normalfont In this case, the left siblings must be taller than the current subtree.}^  
-		   else if(sr != null && cl == null) setRightThread(t,i,sr,mssr);
-		}
+  public static class Tree {
+    double width, height; // Width and height.
+    double x, y, initialX, modifierX, shiftX, posChange;
+    Tree threadLeft, threadRight; // Left and right thread.
+    Tree extremeLeft, extremeRight; // Extreme left and right nodes.
+    double modSumLeft, modSumRight; // Sum of modifiers at the extreme nodes.
+    Tree[] children;
 
-		static void moveSubtree(Tree t, int i, int si, double dist) {
-		   // ^{\normalfont Move subtree by changing mod.}^  
-		   t.c[i].mod+=dist; t.c[i].msel+=dist; t.c[i].mser+=dist;
-		   distributeExtra(t, i, si, dist);                                  
-		}
-		  
-		static Tree nextLeftContour(Tree t) {return t.cs==0 ? t.tl : t.c[0];}
-		static Tree nextRightContour(Tree t){return t.cs==0 ? t.tr : t.c[t.cs-1];} 
-		static double bottom(Tree t) { return t.y + t.h;  }
-		  
-		static void setLeftThread(Tree t, int i, Tree cl, double modsumcl) {
-		   Tree li = t.c[0].el;
-		   li.tl = cl;
-		   // ^{\normalfont Change mod so that the sum of modifier after following thread is correct.}^  
-		   double diff = (modsumcl - cl.mod) - t.c[0].msel ;
-		   li.mod += diff; 
-		   // ^{\normalfont Change preliminary x coordinate so that the node does not move.}^  
-		   li.prelim-=diff;
-		   // ^{\normalfont Update extreme node and its sum of modifiers.}^  
-		   t.c[0].el = t.c[i].el; t.c[0].msel = t.c[i].msel;
-		}
-		  
-		// ^{\normalfont Symmetrical to setLeftThread.}^  
-		static void setRightThread(Tree t, int i, Tree sr, double modsumsr) {
-		   Tree ri = t.c[i].er;
-		   ri.tr = sr;
-		   double diff = (modsumsr - sr.mod) - t.c[i].mser ;
-		   ri.mod += diff; 
-		   ri.prelim-=diff;
-		   t.c[i].er = t.c[i-1].er; t.c[i].mser = t.c[i-1].mser;
-		}
+    Tree(double w, double h, double y, Tree... c) {
+      this.width = w;
+      this.height = h;
+      this.y = y;
+      this.children = c;
+    }
+  }
 
-		static void positionRoot(Tree t) {
-		   // ^{\normalfont Position root between children, taking into account their mod.}^  
-		   t.prelim = (t.c[0].prelim + t.c[0].mod + t.c[t.cs-1].mod + 
-		               t.c[t.cs-1].prelim +  t.c[t.cs-1].w)/2 - t.w/2;
-		}
-		  
-		static void secondWalk(Tree t, double modsum) {
-		   modsum+=t.mod;
-		   // ^{\normalfont Set absolute (non-relative) horizontal coordinate.}^  
-		   t.x = t.prelim + modsum;
-		   addChildSpacing(t);                                               
-		   for(int i = 0 ; i < t.cs ; i++) secondWalk(t.c[i],modsum);
-		}
+  static void layout(Tree tree) {
+    firstWalk(tree);
+    secondWalk(tree, 0);
+  }
 
-		static void distributeExtra(Tree t, int i, int si, double dist) {           
-		   // ^{\normalfont Are there intermediate children?}^
-		   if(si != i-1){                                                    
-		      double nr = i - si;                                            
-		      t.c[si +1].shift+=dist/nr;                                     
-		      t.c[i].shift-=dist/nr;                                         
-		      t.c[i].change-=dist - dist/nr;                                 
-		   }                                                                 
-		}                                                                    
-		 
-		// ^{\normalfont Process change and shift to add intermediate spacing to mod.}^  
-		static void addChildSpacing(Tree t){
-		   double d = 0, modsumdelta = 0;                                    
-		   for(int i = 0 ; i < t.cs ; i++){                                  
-		      d+=t.c[i].shift;                                               
-		      modsumdelta+=d + t.c[i].change;                                
-		      t.c[i].mod+=modsumdelta;                                       
-		   }                                                                 
-		}                                                                    
+  static void firstWalk(Tree tree) {
+    if (tree.children.length == 0) {
+      setExtremes(tree);
+      return;
+    }
+    firstWalk(tree.children[0]);
+    // Create siblings in contour minimal vertical coordinate and
+    // index list.
+    IndexYLowest indexHighest = updateIndexYLowest(bottom(
+        tree.children[0].extremeLeft), 0, null);
+    for (int i = 1; i < tree.children.length; i++) {
+      firstWalk(tree.children[i]);
+      // Store lowest vertical coordinate while extreme nodes still
+      // point in current subtree.
+      double minY = bottom(tree.children[i].extremeRight);
+      seperate(tree, i, indexHighest);
+      indexHighest = updateIndexYLowest(minY, i, indexHighest);
+    }
+    positionRoot(tree);
+    setExtremes(tree);
+  }
 
-		 // ^{\normalfont A linked list of the indexes of left siblings and their lowest vertical coordinate.}^  
-		static class IYL{                                                          
-		   double lowY; int index; IYL nxt;                                 
-		   public IYL(double lowY, int index, IYL nxt) {                         
-		        this.lowY = lowY; this.index = index; this.nxt = nxt;            
-		   }                                                                     
-		 }                                                                       
-		  
-		static IYL updateIYL(double minY, int i, IYL ih) {                         
-		   // ^{\normalfont Remove siblings that are hidden by the new subtree.}^  
-		   while(ih != null && minY >= ih.lowY) ih = ih.nxt;                 
-		   // ^{\normalfont Prepend the new subtree.}^  
-		   return new IYL(minY,i,ih);                                       
-		}         
+  static void setExtremes(Tree tree) {
+    if (tree.children.length == 0) {
+      tree.extremeLeft = tree;
+      tree.extremeRight = tree;
+      tree.modSumLeft = tree.modSumRight = 0;
+    } else {
+      tree.extremeLeft = tree.children[0].extremeLeft;
+      tree.modSumLeft = tree.children[0].modSumLeft;
+      tree.extremeRight = tree.children[tree.children.length - 1].extremeRight;
+      tree.modSumRight = tree.children[tree.children.length - 1].modSumRight;
+    }
+  }
+
+  static void seperate(Tree tree, int i, IndexYLowest indexHighest) {
+    // Right contour node of left siblings and its sum of modfiers.
+    Tree subtreeRight = tree.children[i - 1];
+    double modSumSubtreeRight = subtreeRight.modifierX;
+    // Left contour node of current subtree and its sum of modfiers.
+    Tree contourLeft = tree.children[i];
+    double modSumContourLeft = contourLeft.modifierX;
+    while (subtreeRight != null && contourLeft != null) {
+      if (bottom(subtreeRight) > indexHighest.lowY) {
+        indexHighest = indexHighest.next;
+      }
+      // How far to the left of the right side of sr is the left side of cl?
+      double dist = (modSumSubtreeRight + subtreeRight.initialX + subtreeRight.width);
+      dist = dist - (modSumContourLeft + contourLeft.initialX);
+      if (dist > 0) {
+        modSumContourLeft += dist;
+        moveSubtree(tree, i, indexHighest.index, dist);
+      }
+      double subtreeY = bottom(subtreeRight);
+      double contourY = bottom(contourLeft);
+      // Advance highest node(s) and sum(s) of modifiers
+      if (subtreeY <= contourY) {
+        subtreeRight = nextRightContour(subtreeRight);
+        if (subtreeRight != null) {
+          modSumSubtreeRight += subtreeRight.modifierX;
+        }
+      }
+      if (subtreeY >= contourY) {
+        contourLeft = nextLeftContour(contourLeft);
+        if (contourLeft != null) {
+          modSumContourLeft += contourLeft.modifierX;
+        }
+      }
+    }
+    // Set threads and update extreme nodes.
+    // In the first case, the current subtree must be taller than the left siblings.
+    if (subtreeRight == null && contourLeft != null) {
+      setLeftThread(tree, i, contourLeft, modSumContourLeft);
+    }
+    // In this case, the left siblings must be taller than the current subtree.
+    else if (subtreeRight != null && contourLeft == null) {
+      setRightThread(tree, i, subtreeRight, modSumSubtreeRight);
+    }
+  }
+
+  static void moveSubtree(Tree tree, int i, int sourceIndex, double dist) {
+    // Move subtree by changing mod.
+    tree.children[i].modifierX += dist;
+    tree.children[i].modSumLeft += dist;
+    tree.children[i].modSumRight += dist;
+    distributeExtra(tree, i, sourceIndex, dist);
+  }
+
+  static Tree nextLeftContour(Tree tree) {
+    if (tree.children.length == 0) {
+      return tree.threadLeft;
+    } else {
+      return tree.children[0];
+    }
+  }
+
+  static Tree nextRightContour(Tree tree) {
+    if (tree.children.length == 0) {
+      return tree.threadRight;
+    } else {
+      return tree.children[tree.children.length - 1];
+    }
+  }
+
+  static double bottom(Tree tree) {
+    return tree.y + tree.height;
+  }
+
+  static void setLeftThread(Tree tree, int i, Tree contourLeft, double modSumContourLeft) {
+    Tree leftIndex = tree.children[0].extremeLeft;
+    leftIndex.threadLeft = contourLeft;
+    // Change mod so that the sum of modifier after following thread is correct.
+    double diff = (modSumContourLeft - contourLeft.modifierX);
+    diff = diff - tree.children[0].modSumLeft;
+    leftIndex.modifierX += diff;
+    // Change preliminary x coordinate so that the node does not move.
+    leftIndex.initialX -= diff;
+    // Update extreme node and its sum of modifiers.
+    tree.children[0].extremeLeft = tree.children[i].extremeLeft;
+    tree.children[0].modSumLeft = tree.children[i].modSumLeft;
+  }
+
+  // Symmetrical to setLeftThread.
+  static void setRightThread(Tree tree, int i, Tree subtreeRight, double modSumSubtreeRight) {
+    Tree rightIndex = tree.children[i].extremeRight;
+    rightIndex.threadRight = subtreeRight;
+    double diff = (modSumSubtreeRight - subtreeRight.modifierX);
+    diff = diff - tree.children[i].modSumRight;
+    rightIndex.modifierX += diff;
+    rightIndex.initialX -= diff;
+    tree.children[i].extremeRight = tree.children[i - 1].extremeRight;
+    tree.children[i].modSumRight = tree.children[i - 1].modSumRight;
+  }
+
+  static void positionRoot(Tree tree) {
+    // Position root between children, taking into account their mod.
+    tree.initialX = (tree.children[0].initialX +
+        tree.children[0].modifierX +
+        tree.children[tree.children.length - 1].modifierX +
+        tree.children[tree.children.length - 1].initialX +
+        tree.children[tree.children.length - 1].width) / 2 - tree.width / 2;
+  }
+
+  static void secondWalk(Tree tree, double modSum) {
+    modSum += tree.modifierX;
+    // Set absolute (non-relative) horizontal coordinate.
+    tree.x = tree.initialX + modSum;
+    addChildSpacing(tree);
+    for (int i = 0; i < tree.children.length; i++) {
+      secondWalk(tree.children[i], modSum);
+    }
+  }
+
+  static void distributeExtra(Tree tree, int i, int sourceIndex, double dist) {
+    // Are there intermediate children?
+    if (sourceIndex != i - 1) {
+      double numberRecipients = i - sourceIndex;
+      tree.children[sourceIndex + 1].shiftX += dist / numberRecipients;
+      tree.children[i].shiftX -= dist / numberRecipients;
+      tree.children[i].posChange -= dist - dist / numberRecipients;
+    }
+  }
+
+  // Process change and shift to add intermediate spacing to mod.
+  static void addChildSpacing(Tree tree) {
+    double d = 0, modSumDelta = 0;
+    for (int i = 0; i < tree.children.length; i++) {
+      d += tree.children[i].shiftX;
+      modSumDelta += d + tree.children[i].posChange;
+      tree.children[i].modifierX += modSumDelta;
+    }
+  }
+
+  // A linked list of the indexes of left siblings and their 
+  // lowest vertical coordinate.
+  static class IndexYLowest {
+    double lowY;
+    int index;
+    IndexYLowest next;
+
+    public IndexYLowest(double lowY, int index, IndexYLowest next) {
+      this.lowY = lowY;
+      this.index = index;
+      this.next = next;
+    }
+  }
+
+  static IndexYLowest updateIndexYLowest(double minY, int i, IndexYLowest indexHighest) {
+    // Remove siblings that are hidden by the new subtree.
+    while (indexHighest != null && minY >= indexHighest.lowY) {
+     indexHighest = indexHighest.next;
+    }
+    // Prepend the new subtree.
+    return new IndexYLowest(minY, i, indexHighest);
+  }
 }
